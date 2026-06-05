32d8f6315fca7076827061334cd9a7e36727a3f8
smkn-abs <smkn@abstractit.in>
2026-06-05 09:37:30 +0530

    Add new background features and improvements

--- src/components/3dMode/base.js
+++ src/components/3dMode/base.js
@@ -1,4 +1,6 @@
-import { OrbitControls, useHelper } from '@react-three/drei';
+import { OrbitControls, useHelper, useTexture } from '@react-three/drei';
+import * as THREE from 'three';
+import redMountains from '../../images/red-mountains.png';
 import { React, Suspense, useRef } from 'react';
 import { useThree } from '@react-three/fiber';
 import Targets from './scene/targets';
@@ -18,6 +20,34 @@ import PlayPause from './playPause';
 const x = 2.5;
 const y = 50;
 const z = 1.5;
+const bgPosY = 5;
+const bgPosZ = -50;
+const bgRepeatX = 3;
+const bgRepeatY = 2;
+const bgScaleX = 3;
+const bgScaleY = 3;
+
+const BackgroundMesh = () => {
+	const { viewport } = useThree();
+	const texture = useTexture(redMountains);
+
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set(bgRepeatX, bgRepeatY);
+
+	const meshScaleX = viewport.width * bgScaleX;
+	const meshScaleY = viewport.height * bgScaleY;
+
+	return (
+		<mesh
+			position={ [0, bgPosY, bgPosZ] }
+			scale={ [meshScaleX, meshScaleY, 1] }
+		>
+			<planeGeometry args={ [1, 1] }/>
+			<meshBasicMaterial map={ texture }/>
+		</mesh>
+	);
+};
 // eslint-disable-next-line max-lines-per-function
 const Base = (context) => {
 	const { viewport, mouse } = useThree();
@@ -30,7 +60,7 @@ const Base = (context) => {
 
 	return (
 		<>
-			<color attach="background" args={ ['lightblue'] }/>
+			<color attach="background" args={ ['#1a0505'] }/>
 			<ambientLight color="white" intensity={ 0.3 }/>
 			<directionalLight
 				ref={ ref }
@@ -42,6 +72,7 @@ const Base = (context) => {
 				<Flight { ...enrichedContext }/>
 				<Bullets { ...enrichedContext }/>
 				<Clouds { ...enrichedContext }/>
+				<BackgroundMesh/>
 				<Plane { ...enrichedContext }/>
 				<Bgm { ...enrichedContext }/>
 				<HealthBar { ...enrichedContext }/>
